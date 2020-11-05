646b8dc7cd5be4fd0c5df1855b687e64aaec370b
jonaphael <jonaphael4@gmail.com>
2020-11-05 12:19:41 -0300

    fixed comments on helper

--- test/unit/kafka/Helper.test.js
+++ test/unit/kafka/Helper.test.js
@@ -38,22 +38,15 @@ const resolvedObject = {
 describe('Testing Helper module', () => {
   it('should fail retrieving kafka object status', async () => {
     await expect(Helper.getStatus()).rejects.toThrow();
-  });
-
-  it('should fail retrieving kafka object status', async () => {
-    await expect(Helper.getStatus({})).rejects.toThrow();
+    // an error occurs when getting metadata
+    const consumerObject = new KafkaMock.KafkaConsumer();
+    consumerObject.getMetadata.mockImplementation((abc, callback) => callback(abc));
+    await expect(Helper.getStatus(consumerObject)).resolves.toStrictEqual({connected: false});
   });
 
   it('should resolve because client is connected ', async () => {
     const consumerObject = new KafkaMock.KafkaConsumer();
-    await expect(Helper.getStatus(consumerObject)).resolves.toStrictEqual(resolvedObject);
+    await expect(Helper.getStatus(consumerObject)).resolves.toStrictEqual({connected: true});
   });
 
-
-  it('should rejects due to error', async () => {
-    // an error occurs when getting metadata
-    const consumerObject = new KafkaMock.KafkaConsumer();
-    consumerObject.getMetadata.mockImplementation((abc, callback) => callback(abc));
-    await expect(Helper.getStatus(consumerObject)).rejects.toThrow();
-  });
 });
